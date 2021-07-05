cf78d1ce9329c268bf4a4ce5d3dcc1c6ad8a4950
Lukmanov <ozggldd@gmail.com>
2021-07-05 17:32:34 +0600

    adding map styling

--- src/components/MyMap.js
+++ src/components/MyMap.js
@@ -7,6 +7,33 @@ const MyMap = () => {
   useEffect(() => {
     console.log(dataGeoJson);
   });
+  const styleObj = {
+    fillColor: 'yellow',
+    color: 'black',
+    weight: 2,
+    fillOpacity: 0.1,
+  };
+  const onEachFeature = (feature, layer) => {
+    const countryName = feature.properties.ADMIN;
+    console.log(countryName);
+    layer.bindPopup(countryName);
+    layer.options.color = 'black';
+    let opacity = parseFloat(Math.random().toString().substring(0, 3));
+    console.log('opacity:', opacity);
+    console.log('opacity type:', typeof opacity);
+    layer.setStyle({
+      fillOpacity: opacity,
+    });
+    layer.on({
+      click: (event) => {
+        console.log(event.target.feature.properties.ADMIN);
+        event.target.setStyle({
+          color: 'blue',
+          fillOpacity: 1,
+        });
+      },
+    });
+  };
   return (
     <div>
       <h2 style={{ textAlign: 'center' }}>My Map!</h2>
@@ -15,7 +42,11 @@ const MyMap = () => {
         zoom={10}
         center={[42.884418, 74.576622]}
       >
-        <GeoJSON data={dataGeoJson} />
+        <GeoJSON
+          data={dataGeoJson}
+          style={styleObj}
+          onEachFeature={onEachFeature}
+        />
       </Map>
     </div>
   );
